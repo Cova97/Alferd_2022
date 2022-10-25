34aa88c9cd598a2c6ad8d9606ba48a56140bdfcd
KODA97 <acovamartinez@gmail.com>
2022-10-25 11:51:19 -0500

    Index
    modificación del index

--- app/src/index.js
+++ app/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const name = 'Aldo';
+const name = 'Alferd';
 
-const Jsx = (
-  <>
-    <h1>Hola {name}</h1>
-  </>
-);
+const Jsx = () => {
+  return(
+    <>
+      <h1>Hola {name}</h1>
+    </>
+  );
+};
 
 root.render(<Jsx/>);
 
