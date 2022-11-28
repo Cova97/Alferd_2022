ff4501136613d2c45634632a16c4654ae507bbe0
KODA97 <acovamartinez@gmail.com>
2022-11-28 11:15:42 -0600

    export
    falto un detalle

--- app/src/index.js
+++ app/src/index.js
@@ -4,15 +4,12 @@ import FormLogin from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const name = 'Alferd Enchapados SA de CV';
-
 const Jsx = () => {
   const [inicio, cambiarInicio] = useState(false);
   return(
     <>
       {inicio === true ? (
         <>
-          <h1>Hola {name} </h1>
           <button onClick={() => cambiarInicio(false)}>Cerrar sesión</button>
         </>
       ) : (
